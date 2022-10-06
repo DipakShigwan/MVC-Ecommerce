1cd11c6c69a02da585b611f7b503b59b974014e9
Dipak Shigwan <shigwandipak123@gmail.com>
2022-10-07 01:07:01 +0530

    Stable base
    added connection string and scaffolding

--- E-commerce/E-commerce/Program.cs
+++ E-commerce/E-commerce/Program.cs
@@ -1,8 +1,11 @@
+using E_commerce.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<ApplicationDBContext>(options => options.UseSqlServer("DefaultConnection")); 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
